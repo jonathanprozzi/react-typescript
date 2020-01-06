03fdb555ce02c8e92365effe12d9b5000dece48b
jonathanprozzi <jonathanprozzi@gmail.com>
2020-01-06 17:22:34 -0500

    Added useRef example to Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Input = () => {
   // example using type inference - infers that the input value is a string
   const [name, setName] = useState("");
   // example explicitly setting type (can be string or null)
   // const [name, setName] = useState<string | null>("");
-  return <input value={name} onChange={e => setName(e.target.value)} />;
+
+  // ref is Read Only
+  const ref = useRef<HTMLInputElement>(null!);
+
+  if (ref && ref.current) {
+    console.log("ref", ref.current.value);
+  }
+  return (
+    <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
+  );
 };
 
 export default Input;
